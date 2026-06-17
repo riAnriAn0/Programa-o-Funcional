3a7f6d55d70baf1fae9fb315f72fb76bbec22314
riAnriAn0 <ml2463265@gmail.com>
2026-06-17 13:28:28 -0300

    feat: add function to sort strings by frequency of occurrence
    - Implemented a new function that takes a list of strings and returns a list sorted by the frequency of each string, from most to least frequent.
    - Added example usage in comments for clarity.

---
+++ haskell/av2cc10/prova02.hs
@@ -0,0 +1,7 @@
+1. Imagine  que  você  necessita  realizar  uma  análise  de  dados  transmitidos  ente  dois  dispositivos 
+eletrônicos  em  que  a  frequência  de  repetição  das  informações  é  o  dado  mais  relevante.  Para 
+tanto  você  necessita  implementar  uma  função  que  receba  uma  lista  de  strings  e  retorne  uma 
+lista  com  as  strings  ordenadas  por  frequência  de  aparição  (da  que  mais  se  repete  para  a  que 
+menos se repete). (5,0) 
+Exemplo: 
+[“casa”, “bola”, “casa”, “rua" , “rua”, "casa"]  -> [“casa”, “rua”, “bola"] 
--- haskell/zf/zf.hs
+++ haskell/zf/zf.hs
@@ -1,9 +1,11 @@
 l::[Int]
 l = [1,3,2,5,1,7]
 
+r :: [Int]
 r = [2*a | a <- l]
 
-p = [a | a<-l , mod a 2 ==0]
+p :: [Int]
+p = [a | a<-l , even a]
 
 -- quickSort com ZF
 quickSort::[Int]->[Int]
@@ -11,20 +13,21 @@ quickSort [] = []
 quickSort (a:xs) = quickSort x1 ++ [a] ++ quickSort x2
     where
         x1 = quickSort [y | y <- xs, y <= a]
-        x2 = quickSort [y | y <- xs, y > a] 
-        
+        x2 = quickSort [y | y <- xs, y > a]
+
 
 -- ▪Fatores - Lista os divisores de um número
 fatores::Int->[Int]
-fatores 0 = [] 
+fatores 0 = []
 fatores n = [x | x <- [1..(n-1)] , mod n x == 0]
 
 -- ▪Perfeitos - Lista de números perfeitos de 1 até um número inteiro informado
 nPerft::Int->[Int]
 nPerft 0 = [0]
-nPerft n = [x | x <- [1..n], sum (fatores x) == x ] 
+nPerft n = [x | x <- [1..n], sum (fatores x) == x ]
 
 -- ▪Concatena - Semelhante a função concat (concatena uma lista de listas em uma única)
 concatena :: [[Int]] -> [Int]
 concatena []     = []
 concatena lista = [x | y <- lista , x <- y]
+
