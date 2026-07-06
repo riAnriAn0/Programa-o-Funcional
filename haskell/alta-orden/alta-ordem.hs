b3e0caabafc4d5b456693788f956a9d80513c36d
riAnriAn0 <ml2463265@gmail.com>
2026-07-06 10:32:15 -0300

    refactor: improve function type signatures and add missing cases for value checks

--- haskell/alta-orden/alta-ordem.hs
+++ haskell/alta-orden/alta-ordem.hs
@@ -1,4 +1,5 @@
-l::[Int] = [1,2,3,4,5,6,7,8,9,10]
+l :: [Int]
+l = [1,2,3,4,5,6,7,8,9,10]
 
 -- ▪Defina funções que tomem uma lista de inteiros L e
 
@@ -15,19 +16,42 @@ somaQuadrado n = sum (listaL n)
     -- ▪Verifique se todos os elementos da lista são positivos
 
 isPositivo::[Int]->Bool
-isPositivo = all (>0) 
+isPositivo = all (>0)
 
 -- ▪Escreva funções que calculem
 
     -- ▪O valor mínimo de uma função aplicada a uma lista de inteiros de 0 a n  
 
-valorMin::([Int]->[Int])->[Int]->Int
-valorMin _ [] = 0
-valorMin f l = 
+valorMin::(Int->Int)->Int->Int
+valorMin f n = minimum (map f [0..n])
 
 
     -- ▪Teste se o resultado de uma função f aplicada sobre as entradas de 0 a n são todas iguais
 
+isAllEqual :: (Int -> Int) -> Int -> Bool
+isAllEqual f n = all (== first) resultados
+  where
+    resultados = map f [0..n]
+    first   = head resultados
+
+
     -- ▪Teste se todos os valores de f aplicada às entradas de 0 a n são maiores ou iguais a 0
 
+isAllMaiorIgualZero::(Int->Int)->Int->Bool
+isAllMaiorIgualZero f n = all ( >= 0) result 
+        where 
+            result = map f [0..n]
+
     -- ▪Teste se todos os valores de f aplicada às entradas de 0 a n estão em ordem crescente
+
+isCrescente::[Int]->Bool
+isCrescente [] = True
+isCrescente [x] = True
+isCrescente (x:y:xs) 
+    | y >= x = isCrescente (y:xs)
+    | otherwise = False
+
+isOrdemCrescente::(Int->Int)->Int->Bool
+isOrdemCrescente f n = isCrescente result
+    where   
+        result = map f [0..n]
