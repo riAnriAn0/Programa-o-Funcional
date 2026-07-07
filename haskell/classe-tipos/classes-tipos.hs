286292f3e093cf88c3407fb2f4286ae815bccdb7
riAnriAn0 <ml2463265@gmail.com>
2026-07-07 11:16:27 -0300

    refactor: adjust indentation for getLocation method in Located class

--- haskell/classe-tipos/classes-tipos.hs
+++ haskell/classe-tipos/classes-tipos.hs
@@ -1,10 +1,10 @@
 class Located a where
-getLocation :: a -> (Int, Int)
+    getLocation :: a -> (Int, Int)
 
 data Point = Point { coordX::Int, coordY::Int}
 
 instance Located Point where
-getLocation p = (coordX p, coordY p)
+    getLocation p = (coordX p, coordY p)
 
 origem:: (Located t)=> t -> Bool
 origem p = getLocation p == (0,0)
