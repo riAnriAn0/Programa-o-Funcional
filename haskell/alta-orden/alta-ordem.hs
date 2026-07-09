78267372b3e319637ae6ea3c17a143ad59f660bc
riAnriAn0 <ml2463265@gmail.com>
2026-07-09 10:22:33 -0300

    fix: correct parameter order in filtraPar function

--- haskell/alta-orden/alta-ordem.hs
+++ haskell/alta-orden/alta-ordem.hs
@@ -68,5 +68,5 @@ contaVizinhosIguais (a:xs)
 contador::(a->Bool)->[a]->Int
 contador f l = length (filter f l)
 
-filtraPar::(t->Bool)->(u->Bool)->[t]->[u]->[(t,u)]
-filtraPar f g l1 l2 = [(x,y) | x <- filter f l1 , y <- filter g l2]
+filtraPar::(t->Bool)->[t]->(u->Bool)->[u]->[(t,u)]
+filtraPar f l1 g l2 = [(x,y) | x <- filter f l1 , y <- filter g l2]
