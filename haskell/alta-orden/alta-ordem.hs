695c0db7a413e20e472b63aaf348d1a1a4ebe7e9
riAnriAn0 <ml2463265@gmail.com>
2026-07-09 14:51:56 -0300

    refactor: clean up formatting and indentation in various functions

--- haskell/alta-orden/alta-ordem.hs
+++ haskell/alta-orden/alta-ordem.hs
@@ -38,8 +38,8 @@ isAllEqual f n = all (== first) resultados
     -- ▪Teste se todos os valores de f aplicada às entradas de 0 a n são maiores ou iguais a 0
 
 isAllMaiorIgualZero::(Int->Int)->Int->Bool
-isAllMaiorIgualZero f n = all ( >= 0) result 
-        where 
+isAllMaiorIgualZero f n = all ( >= 0) result
+        where
             result = map f [0..n]
 
     -- ▪Teste se todos os valores de f aplicada às entradas de 0 a n estão em ordem crescente
@@ -47,23 +47,24 @@ isAllMaiorIgualZero f n = all ( >= 0) result
 isCrescente::[Int]->Bool
 isCrescente [] = True
 isCrescente [x] = True
-isCrescente (x:y:xs) 
+isCrescente (x:y:xs)
     | y >= x = isCrescente (y:xs)
     | otherwise = False
 
 isOrdemCrescente::(Int->Int)->Int->Bool
 isOrdemCrescente f n = isCrescente result
-    where   
+    where
         result = map f [0..n]
 
--- aplicacaoExclusiva::(a->Bool)->(a->a->c)->[a]->[a]->[u]
+aplicacaoExclusiva::(t->Bool)->(t->t->u)->[t]->[t]->[u]
+aplicacaoExclusiva f1 f2 l1 l2 = [f2 e1 e2 | e1 <- filter f1 l1, e2 <- filter f1 l2]
 
 contaVizinhosIguais::(Eq t) => [t]->Int
 contaVizinhosIguais [] = 0
 contaVizinhosIguais [a] = 0
 contaVizinhosIguais (a:xs)
     | a == head xs = 1 + contaVizinhosIguais xs
-    | otherwise = contaVizinhosIguais xs 
+    | otherwise = contaVizinhosIguais xs
 
 contador::(a->Bool)->[a]->Int
 contador f l = length (filter f l)
@@ -74,4 +75,3 @@ filtraPar f g l1 l2 = [(x,y) | x <- filter f l1 , y <- filter g l2]
 
 selecionaExecuta::(t->Bool)->(t->u)->[t]->[u]
 selecionaExecuta f g l = map g (filter f l)
- 
